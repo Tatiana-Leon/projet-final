731c53e2cb6db15637703c3dc849febf08b62dbf
Tatiana-Leon <tatianabernalleon@Tatianas-MacBook-Air.local>
2023-07-03 19:23:14 -0400

    test wrapper blurb

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -29,4 +29,36 @@ $( document ).ready(function() {
 	nav();
 	
 	});
-	
+
+
+//2.  slider
+	document.addEventListener('DOMContentLoaded', function() {
+		var slider = document.getElementById('full-slide');
+		var slides = Array.from(slider.querySelectorAll('.banner li'));
+		var currentIndex = slides.findIndex(function(slide) {
+		  return slide.classList.contains('active');
+		});
+	  
+		function showSlide(index) {
+		  slides.forEach(function(slide) {
+			slide.classList.remove('active');
+		  });
+	  
+		  slides[index].classList.add('active');
+		}
+	  
+		function nextSlide() {
+		  currentIndex = (currentIndex + 1) % slides.length;
+		  showSlide(currentIndex);
+		}
+	  
+		function prevSlide() {
+		  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+		  showSlide(currentIndex);
+		}
+	  
+		slider.querySelector('.next').addEventListener('click', nextSlide);
+		slider.querySelector('.prev').addEventListener('click', prevSlide);
+	  
+		showSlide(currentIndex);
+	  });
