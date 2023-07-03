6e895c4e140cffaf3fae9b463a612118fdd38304
Tatiana-Leon <tatianabernalleon@Tatianas-MacBook-Air.local>
2023-07-03 16:51:31 -0400

    ajout de map

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,15 +1,63 @@
+//1.  menu-fluide
 $( document ).ready(function() {
 	
-function nav(){
+	function nav(){
+		
+		$('.nav-toggle').click(function(){
+			
+			$('.nav').toggleClass('open');
+			
+		});
 	
-	$('.nav-toggle').click(function(){
+		$('.nav a').click(function(event) {
+			event.preventDefault();
+	  
+			const targetId = $(this).attr('href');
+			const targetPosition = $(targetId).offset().top;
+	  
+			$('html, body').animate(
+			  {
+				scrollTop: targetPosition
+			  },
+			  'fluide',
+			);
+		  });
 		
-		$('.nav').toggleClass('open');
-		
+	}
+	
+	nav();
+	
 	});
 	
-}
-
-nav();
-
-});
+	//2.  slider
+	
+	document.addEventListener('DOMContentLoaded', function() {
+		var slider = document.getElementById('full-slide');
+		var slides = Array.from(slider.querySelectorAll('.banner li'));
+		var currentIndex = slides.findIndex(function(slide) {
+		  return slide.classList.contains('active');
+		});
+	  
+		function showSlide(index) {
+		  slides.forEach(function(slide) {
+			slide.classList.remove('active');
+		  });
+	  
+		  slides[index].classList.add('active');
+		}
+	  
+		function nextSlide() {
+		  currentIndex = (currentIndex + 1) % slides.length;
+		  showSlide(currentIndex);
+		}
+	  
+		function prevSlide() {
+		  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+		  showSlide(currentIndex);
+		}
+	  
+		slider.querySelector('.next').addEventListener('click', nextSlide);
+		slider.querySelector('.prev').addEventListener('click', prevSlide);
+	  
+		showSlide(currentIndex);
+	  });
