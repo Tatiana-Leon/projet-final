d07c98109893bf3b4a4b90df010efe946ee0e839
Tatiana-Leon <tatianabernalleon@Tatianas-MacBook-Air.local>
2023-07-03 17:53:52 -0400

    test commit java

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,4 @@
+
 //1.  menu-fluide
 $( document ).ready(function() {
 	
@@ -29,35 +30,3 @@ $( document ).ready(function() {
 	
 	});
 	
-	//2.  slider
-	
-	document.addEventListener('DOMContentLoaded', function() {
-		var slider = document.getElementById('full-slide');
-		var slides = Array.from(slider.querySelectorAll('.banner li'));
-		var currentIndex = slides.findIndex(function(slide) {
-		  return slide.classList.contains('active');
-		});
-	  
-		function showSlide(index) {
-		  slides.forEach(function(slide) {
-			slide.classList.remove('active');
-		  });
-	  
-		  slides[index].classList.add('active');
-		}
-	  
-		function nextSlide() {
-		  currentIndex = (currentIndex + 1) % slides.length;
-		  showSlide(currentIndex);
-		}
-	  
-		function prevSlide() {
-		  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-		  showSlide(currentIndex);
-		}
-	  
-		slider.querySelector('.next').addEventListener('click', nextSlide);
-		slider.querySelector('.prev').addEventListener('click', prevSlide);
-	  
-		showSlide(currentIndex);
-	  });
